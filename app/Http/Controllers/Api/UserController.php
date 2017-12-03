203365581546f2683d60f1c881567194d9e0087b
dapa <416621723@qq.com>
2017-12-03 18:14:54 +0800

    'update'

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -6,6 +6,7 @@ use App\Http\Common\Message;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Mockery\Exception;
 
 class UserController extends Controller
@@ -23,17 +24,20 @@ class UserController extends Controller
             $user_info = DB::table($this->table)
                 ->where('email', $request['email'])
                 ->first();
-            if (decrypt($request['password'] == $user_info->password)) {
-                session('user_info', [
+            if (empty($user_info))
+                Message::jsonMsg(500, 'user is not exists');
+
+            if (Hash::check($request['password'], $user_info->password)) {
+                session(['user_info' => [
                     'id' => $user_info->id,
                     'name' => $user_info->name,
                     'avatar_b_url' => $user_info->avatar_b_url,
                     'avatar_m_url' => $user_info->avatar_m_url,
                     'avatar_s_url' => $user_info->avatar_s_url,
-                ]);
-                Message::jsonMsg(200, true);
+                ]]);
+                Message::jsonMsg(200, 'login success');
             }
-            Message::jsonMsg(500, 'Username or password is incorrect');
+            Message::jsonMsg(500, 'password is incorrect');
         } catch (Exception $e) {
             Message::jsonMsg(500, 'failed');
         }
@@ -50,20 +54,20 @@ class UserController extends Controller
                 Message::jsonMsg(500, 'please input your name');
             $data = [
                 'email' => $request->email,
-                'password' => encrypt($request->password),
+                'password' => Hash::make($request->password),
                 'name' => $request->name,
             ];
             $user = DB::table($this->table)
                 ->where('email', $request->email)
                 ->orWhere('name', $request->name)
                 ->get();
-            if (!empty($user))
-                Message::jsonMsg(500, 'email or name has already there');
+            if (!$user->isEmpty())
+                Message::jsonMsg(500, 'email or name is already used');
             $id = DB::table($this->table)
                 ->insertGetId($data);
             if (empty($id))
                 Message::jsonMsg(500, 'failed');
-            Message::jsonMsg(200, true);
+            Message::jsonMsg(200, 'signup success');
         } catch (Exception $e) {
             Message::jsonMsg(500, 'failed');
         }
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test', function () {
-    return 'test';
-});
+Route::get('/test', 'TestController@login');
